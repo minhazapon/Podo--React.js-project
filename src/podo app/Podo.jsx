520303f7ab7237fba3d8bee03083f484cc61eb8f
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-16 03:19:42 -0800

    added date and time work

--- src/podo app/Podo.jsx
+++ src/podo app/Podo.jsx
@@ -1,17 +1,42 @@
+import { useEffect } from "react"
+import { useState } from "react"
 
 function Podo() {
+
+
+    ///date work///
+    const [date, setDate] = useState("")
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const formatDate = now.toLocaleDateString()
+            const formatTime = now.toLocaleTimeString()
+            setDate(` ${formatDate} - ${formatTime} `)
+        }, 1000)
+        return () => clearInterval(interval)
+    }, [])
+    ///date work///
+
+
     return (
         <div className=" m-20 ">
             <div>
                 <div>
                     <p className=" text-center text-green-500 text-5xl font-bold ">Podo List</p>
-                    <p className=" text-center text-green-500 font-thin mt-5 ">Date and Time</p>
+                    <p className=" text-center text-green-500 font-thin mt-5 "> {date} </p>
                 </div>
                 <div className=" mt-6 ">
                     <form>
                         <div className=" flex justify-center items-center ">
-                            <input className=" border-[1px] h-[50px] w-[400px] p-3 rounded-l-[20px] font-serif " placeholder="Add Your Task" type="text" />
-                            <input className=" bg-green-500 h-[50px] p-3 w-[100px] rounded-r-[20px] font-serif text-white  " type="submit" value="add" />
+                            <input
+                                className=" border-[1px] h-[50px] w-[400px] p-3 rounded-l-[20px] font-serif "
+                                placeholder="Add Your Task"
+                                type="text" />
+                            <input
+                                className=" bg-green-500 h-[50px] p-3 w-[100px] rounded-r-[20px] font-serif text-white"
+                                type="submit"
+                                value="add" />
                         </div>
                     </form>
                 </div>
