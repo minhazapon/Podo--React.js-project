9785d94c2262f4bd709af726f0d840caaa2df577
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-16 02:03:01 -0800

    added file

--- src/podo app/Podo.jsx
+++ src/podo app/Podo.jsx
@@ -10,7 +10,7 @@ function Podo() {
                     <p className=" text-center text-green-500 text-5xl font-bold ">Podo List</p>
                     <p className=" text-center text-green-500 font-thin mt-5 ">Date and Time</p>
                 </div>
-                <div className=" mt-10 ">
+                <div className=" mt-6 ">
                     <form>
                         <div className=" flex justify-center items-center ">
                             <input className=" border-[1px] h-[50px] w-[400px] p-3 rounded-l-[20px] font-serif " placeholder="Add Your Task" type="text" />
@@ -18,7 +18,14 @@ function Podo() {
                         </div>
                     </form>
                 </div>
+                <div>
+                    <div>
 
+                    </div>
+                </div>
+                <div className=" flex justify-center mt-8 ">
+                    <button className=" btn bg-red-600 text-white ">Clear all</button>
+                </div>
             </div>
         </div>
     )
